9fc40eee29588b851889271231b18170834415a5
Davide Facoetti <davide.facoetti2@faculty.ai>
2023-11-15 05:59:37 +0000

    2016-06

--- 2016/2016-06_solution.py
+++ 2016/2016-06_solution.py
@@ -1,18 +1,33 @@
+from collections import Counter
+
+
 def read_input(input_file) -> list[str]:
     with open(input_file) as f:
-        return f.readlines()
+        return f.read().splitlines()
 
 
-def part1(input: list[str]) -> ...:
-    ...
+def part1(input: list[str]) -> str:
+    n_col = len(input[0])
+    assert all(len(line) == n_col for line in input)
+    message = ""
+    for col in range(n_col):
+        counter = Counter(line[col] for line in input)
+        message += counter.most_common(1)[0][0]
+    return message
 
 
-def part2(input: list[str]) -> ...:
-    ...
+def part2(input: list[str]) -> str:
+    n_col = len(input[0])
+    assert all(len(line) == n_col for line in input)
+    message = ""
+    for col in range(n_col):
+        counter = Counter(line[col] for line in input)
+        message += counter.most_common()[-1][0]
+    return message
 
 
 def main() -> None:
-    input = read_input("2016-05_input.txt")
+    input = read_input("2016-06_input.txt")
     print("Part 1: ")
     print("-------")
     print(part1(input))
