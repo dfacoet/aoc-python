5b726a71a93a392cfc5d05e1f773bdda611b7874
dfacoet <11651258+dfacoet@users.noreply.github.com>
2023-11-16 23:49:47 +0000

    2016-11 (pen and paper)

---
+++ 2016/2016-11_solution.py
@@ -0,0 +1,32 @@
+# Input state
+
+#########################
+# 4 # .  .. .. .. .. .. #
+# 3 # .  .. .. .. .. .. #
+# 2 # .  .M .M .. .. .. #
+# 1 # E  G. G. GM GM GM #
+#########################
+
+
+# Part 1
+
+# The best you can do is to always take two items up and one down.
+# To move all items up one floor, you need at least 2 * (10 - 2) + 1 = 17 moves.
+# This lower bound is tight:
+# For each pair, take both up and go back with the chip (10 moves)
+# Then go up with 2 chips and back with 1 three times (16) and
+# finally take the last two up (17).
+
+# In the initial configuration, two items are already on the top floor,
+# saving up to 4 moves -> need at least 13 moves to get all on the second floor.
+# Again, this lower bound is tight:
+# Take the unpaired generators up, go back with a chip (2 moves)
+# Then proceed as before with 2 * (7 - 2) + 1 = 11 moves (13 total).
+
+# Therefore the minimum number of moves is 13 + 2 * 17 = 47.
+
+
+# Part 2
+
+# There are two extra matched pairs on the first floor, adding
+# a total of 3 * 2 * 4 = 24 moves, so the total is 71.
